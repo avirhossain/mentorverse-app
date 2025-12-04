8ff939ce2305e7e894da72ae1e45474fb70a6df1
Md. Mazharul Hossain <avirhossain@gmail.com>
2025-12-04 07:27:22 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9116365139-51ee1",
+  "appId": "1:992447208758:web:889bed46c911e4041c2797",
+  "apiKey": "AIzaSyBlFUHfwADRcmPam8Lt8PZ8miYfVtRqcf0",
+  "authDomain": "studio-9116365139-51ee1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "992447208758"
+};
