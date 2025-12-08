b4578c557ea82584704cfd6ab869dbea3b6ecbaf
Md. Mazharul Hossain <avirhossain@gmail.com>
2025-12-08 16:41:21 +0000

    This tells us that the current rules in firestore.rules do not allow a l

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { Sidebar } from '@/components/admin/Sidebar';
 import { Header } from '@/components/common/Header';
+import { AdminAuthProvider } from '@/firebase/auth/admin-auth-provider';
 
 export default function AdminLayout({
   children,
@@ -9,14 +10,16 @@ export default function AdminLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex min-h-screen w-full flex-col bg-muted/40">
-      <Sidebar />
-      <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
-        <Header />
-        <main className="flex-1 gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
-          {children}
-        </main>
+    <AdminAuthProvider>
+      <div className="flex min-h-screen w-full flex-col bg-muted/40">
+        <Sidebar />
+        <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+          <Header />
+          <main className="flex-1 gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
+            {children}
+          </main>
+        </div>
       </div>
-    </div>
+    </AdminAuthProvider>
   );
 }
