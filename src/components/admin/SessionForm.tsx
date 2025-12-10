dcc964ad0a2c05669058a6cf57e2891b1b6fedf3
Md. Mazharul Hossain <avirhossain@gmail.com>
2025-12-10 04:51:58 +0000

    unable to pick a date and time from create session

--- src/components/admin/SessionForm.tsx
+++ src/components/admin/SessionForm.tsx
@@ -221,7 +221,7 @@ export const SessionForm: React.FC<SessionFormProps> = ({
                         selected={field.value}
                         onSelect={field.onChange}
                         disabled={(date) =>
-                          date < new Date() || date < new Date('1900-01-01')
+                          date < new Date(new Date().setHours(0,0,0,0))
                         }
                         initialFocus
                       />
